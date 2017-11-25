398f739b217c2bd96cf581f0c33e5ddb65e97476
Vasyl-Tserpiak <cerpiakvasia@gmail.com>
2017-11-25 17:21:30 +0200

    design create chanel

--- teamssd/Data/Entities/Chanel.cs
+++ teamssd/Data/Entities/Chanel.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Payzaar.Core.Data.Abstract;
+using teamssd.Data.Enum;
 
 namespace teamssd.Data.Entities
 {
     public class Chanel : Entity<int>
     {
+        [DisplayName ("Назва каналу")]
         public string Name { get; set; }
-        public string Text { get; set; }
+        [DisplayName("Опис")]
+        public string Description { get; set; }
+
+        public ChanelType ChanelType { get; set; }
+        public string Location { get; set; }
 
         public string OwnerId { get; set; }
         public virtual ApplicationUser Owner { get; set; }
---
+++ teamssd/Data/Enum/ChanelType.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace teamssd.Data.Enum
+{
+    public enum ChanelType
+    {
+        [Display(Name = "Новини дня")]
+        NewsDay = 0,
+        [Display(Name = "Політика")]
+        Policy = 1,
+        [Display(Name = "Економіка")]
+        Economy = 2,
+        [Display(Name = "Розваги")]
+        Entertainment = 3,
+        [Display(Name = "Надзвичайні ситуації")]
+        Emergency = 4,
+        [Display(Name = "Спорт")]
+        Sport = 5,
+        [Display(Name = "Автомобілі")]
+        Cars = 6,
+        [Display(Name = "Здоров'я")]
+        Health = 7,
+        [Display(Name = "Визначні особи")]
+        ProminentPersons = 8,
+        [Display(Name = "Світ")]
+        World = 9,
+        [Display(Name = "Господарство")]
+        Household = 10,
+        [Display(Name = "Погода")]
+        Weather = 11
+
+    }
+}
