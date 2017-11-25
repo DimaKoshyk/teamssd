954130ecf0b96c97c487a0834183a5b862e466f0
kosh <cerpiakvasia@gmail.com>
2017-11-25 17:31:09 +0200

    commit firts of dashboard

--- teamssd/Controllers/HomeController.cs
+++ teamssd/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using teamssd.Controllers.Abstract;
 
 namespace teamssd.Controllers
 {
---
+++ teamssd/Models/DashboardViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using teamssd.Data.Entities;
+
+namespace teamssd.Models
+{
+    public class DashboardViewModels
+    {
+        public IList<Chanel> Chanels { get; set; }
+
+        public IList<News> NewsOfFirstChanel { get; set; }
+
+        //public IList<RecommendChanel> RecommendChanels { get; set; }
+    }
+}
