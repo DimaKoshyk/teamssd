0df29377cc018a0ed8b1405347b7a282f8722ced
Vasyl-Tserpiak <cerpiakvasia@gmail.com>
2017-11-25 20:49:23 +0200

    q

---
+++ teamssd/Helpers/ImageHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace teamssd.Helpers
+{
+    public class ImageHelper
+    {
+        //public string ConvertToBase64String()
+        //{
+        //    using (Image image = Image.FromFile(Path))
+        //    {
+        //        using (MemoryStream m = new MemoryStream())
+        //        {
+        //            image.Save(m, image.RawFormat);
+        //            byte[] imageBytes = m.ToArray();
+
+        //            // Convert byte[] to Base64 String
+        //            string base64String = Convert.ToBase64String(imageBytes);
+        //            return base64String;
+        //        }
+        //    }
+        //}
+    }
+}
